1d455d62671c399c704f63128351ded1fe05634d
vaibhavbudhia <vaibhavbudhia7306@gmail.com>
2026-05-01 16:19:01 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ 
+    baseURL: 'https://parking-system-production-9733.up.railway.app/api' 
+});
 
-// attach token to every request automatically
 API.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
     if (token) config.headers['authorization'] = token;
